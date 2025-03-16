e96cffcccdb295c06a59281ba578c8e887cd500b
Armand Winant <bastien.winant@mail.mcgill.ca>
2025-03-16 22:45:01 +0100

    terraform s3 bucket

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,12 @@ variable "region" {
   description = "Project region"
   default     = "eu-central-1"
 }
+
+resource "aws_s3_bucket" "utd19_bucket" {
+  bucket = "utd19"
+  force_destroy = true
+
+  tags = {
+    Name        = "UTD19 data"
+  }
+}
