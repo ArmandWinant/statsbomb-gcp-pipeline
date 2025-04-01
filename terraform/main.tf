bc49a6e345e20a83999549030c05d102ec571b9d
Armand Winant <bastien.winant@mail.mcgill.ca>
2025-04-02 00:00:18 +0200

    create terraform gcp buckets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,35 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.27.0"
     }
   }
 }
 
 provider "google" {
-  project = var.location
-  region = var.region
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.credentials)
+}
+
+resource "google_storage_bucket" "data-bucket" {
+  name          = var.gcs_data_bucket
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_storage_bucket" "code-bucket" {
+  name          = var.gcs_code_bucket
+  location      = var.location
+  force_destroy = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,29 @@
 variable "project_id" {
-  default = "utd19-455521"
+  default     = "utd19-455521"
   description = "Project ID"
 }
 
 variable "credentials" {
-  default = "./.keys/service-account-creds.json"
+  default     = "./.keys/service-account-creds.json"
   description = "Service account credential file location"
 }
 
 variable "location" {
-  default = "EU"
+  default     = "EU"
   description = "Project location"
 }
 
 variable "region" {
-  default = "europe-west3"
+  default     = "europe-west3"
   description = "Project region"
 }
 
 variable "gcs_code_bucket" {
-  default = "utd19-455521-code"
+  default     = "utd19-455521-code"
   description = "Bucket for code files "
 }
 
 variable "gcs_data_bucket" {
-  default = "utd19-455521-data"
+  default     = "utd19-455521-data"
   description = "Bucket for raw and processed data"
 }
